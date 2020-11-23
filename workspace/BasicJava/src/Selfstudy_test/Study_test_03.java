481cb0d152d46e27e2455f555b183c9de11cab07
Shane_Park <kkobuk@hotmail.co.kr>
2020-11-23 11:53:26 +0900

    first commit

---
+++ workspace/BasicJava/src/Selfstudy_test/Study_test_03.java
@@ -0,0 +1,15 @@
+package Selfstudy_test;
+
+public class Study_test_03 {
+	public static void main(String[] args) {
+		
+		int a = 010;
+		int b = 0x10;
+		int c = 0b10;
+		
+		System.out.println(a+" "+b+" "+c+" ");
+		
+		
+	}
+
+}
---
+++ workspace/BasicJava/src/Selfstudy_test/Test_git.java
@@ -0,0 +1,12 @@
+package Selfstudy_test;
+
+public class Test_git {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("test 2");
+		System.out.println("test 3");
+	
+	}
+
+}
