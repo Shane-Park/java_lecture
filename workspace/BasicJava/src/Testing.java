624d4b94834d40b3a5b70e03c259f96a65b253ba
Shane_Park <kkobuk@hotmail.co.kr>
2020-11-27 21:10:15 +0900

    last day

--- workspace/BasicJava/src/Testing.java
+++ workspace/BasicJava/src/Testing.java
@@ -1,28 +1,15 @@
+import java.util.Scanner;
+
 
 public class Testing {
 	public static void main(String[] args) {
-		int n = 3;
-		int m = 12;
-		int[] answer = new int[2];
-		answer[0] = 1;
-        if(n>m){
-            int temp = n;
-            n = m;
-            m = temp;
-        }
-        
-        for(int i=2; i<=n; i++){
-            if(n%i==0 && m%i==0)
-                answer[0] = i;
-        }
-                
-        answer[1] = n * m / answer[0];
-       
-        System.out.println(n+" "+m+" "+answer[0]+" "+answer[1]);
-        
-    }
+	
+		
+		
 	}
 	
+}
+
 
 
 
