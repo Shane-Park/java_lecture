c1069dce87cf943edae14fd2de46c95ca4c75f75
Shane <psh40963@naver.com>
2020-11-29 20:37:30 +0900

    Set comma editted

--- workspace/BasicJava/src/d_array/Homework5_SetComma2.java
+++ workspace/BasicJava/src/d_array/Homework5_SetComma2.java
@@ -2,12 +2,9 @@ package d_array;
 
 import java.util.Scanner;
 
-public class Homework5_SetComma {
+public class Homework5_SetComma2 {
 
 	public static void main(String[] args) {
-		// 숫자를 입력받아 입력 받은 숫자에 3자리 마다 콤마(,)를 붙여 출력해주세요.
-		// 10000000 -> 10,000,000 
-		// 숫자를 입력 받을때는 string type 으로 입력받으세요 
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("숫자를 입력 해 주세요 : ");
@@ -19,7 +16,6 @@ public class Homework5_SetComma {
 				str2.append(",");               // 숫자길이-i 가 3으로 나누어 떨어질 떄마다		//
 			str2.append(str.charAt(i));         // 쉼표를 찍습니다						//
 		}                                       //////////////////////////////////
-				
 		
 		System.out.println(str2);
 
